86c21d60a291a04760e34ed98c4ee84158b242d1
Connor O'Brien <cobrien.js@gmail.com>
2023-08-28 11:05:59 -0500

    Starting Directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import getElement from './get-element/getElement';
 import waitForConditions from './wait-for-conditions/waitForConditions';
 import onUrlChange from './on-SPA-url-change/onUrlChange';
+import logError from './log-error/logError';
 
-export { getElement, waitForConditions, onUrlChange };
+export { getElement, waitForConditions, onUrlChange, logError };

6706b3d18c461ce39292a1bf14f386520ac80a63
Connor O'Brien <cobrien.js@gmail.com>
2023-09-05 17:09:51 -0600

    Adding Initial Class and Operators

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import getElement from './get-element/getElement';
-import waitForConditions from './wait-for-conditions/waitForConditions';
-import onUrlChange from './on-SPA-url-change/onUrlChange';
-import logError from './log-error/logError';
+import getElement from './get-element';
+import waitForConditions from './wait-for-conditions';
+import onUrlChange from './on-SPA-url-change';
+import logError from './log-error';
 
-export { getElement, waitForConditions, onUrlChange, logError };
+export default class FEUtils {
+	constructor({ env = 'PROD', api }) {
+		this.env = env;
+		this.api = api;
+
+		if (this.env !== 'DEV') {
+			console.log = () => {};
+			console.warn = () => {};
+			console.info = () => {};
+			console.error = () => {};
+		}
+	}
+
+	getElement = getElement.bind(this);
+	waitForConditions = waitForConditions.bind(this);
+	onUrlChange = onUrlChange.bind(this);
+	logError = logError.bind(this);
+}

e5a5d079f54fe0cb3f9b5fb986b7200ad2a81452
Connor O'Brien <cobrien.js@gmail.com>
2024-02-05 14:34:29 -0600

    Adding failsafe try / catch

--- src/log/log.js
+++ src/log/log.js
@@ -1,14 +1,17 @@
 const log = (message, method = 'log') => {
-	if (
-		/fecli|QA_test|debug_mode|FE_LOADER|jump/.test(location.href) ||
-		window['FeActivityLoader'].detectTypeOfEnvironment() == 'DEV' ||
-		location.href.indexOf('hc9t07003') >= 0 ||
-		location.href.indexOf('itg.buy.hpe.com') >= 0
-	) {
-		// header/content separation
-		let argsArray = [...arguments];
-		argsArray.shift()
-		console?.[method](message, argsArray)
+	try {
+		if (
+			/fecli|QA_test|debug_mode|FE_LOADER|jump/.test(window.location.href) ||
+			window?.['FeActivityLoader']?.detectTypeOfEnvironment() === 'DEV' ||
+			window?.['FeActivityLoader']?.detectTypeOfEnvironment() === 'QA'
+		) {
+			// header/content separation
+			let argsArray = [...arguments];
+			argsArray.shift()
+			console?.[method](message, argsArray)
+		}
+	} catch (err) {
+		// Do nothing
 	}
 };
 

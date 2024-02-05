67100a05be57d955fb6d5bfa963c4044074cd210
Connor O'Brien <cobrien.js@gmail.com>
2024-02-05 16:06:01 -0600

    Export log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import waitForConditions from './wait-for-conditions/waitForConditions';
 import onUrlChange from './on-SPA-url-change/onUrlChange';
 import cookie from './cookie-manager/cookie';
 import exitIntent from './exit-intent/exitIntent';
-import observeDom from './observe-dom/observeDom'
+import observeDom from './observe-dom/observeDom';
+import log from './log/log';
 
-export { getElement, waitForConditions, onUrlChange, exitIntent, observeDom, cookie };
+export { getElement, waitForConditions, onUrlChange, exitIntent, observeDom, cookie, log };
